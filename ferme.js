3798bfc57e289b4c661f6b4857a0f2d634e8b847
Razafimamonjisoa Mendrika Harizo <98638405+Mendrika27@users.noreply.github.com>
2022-03-19 12:12:22 +0300

    feat: add commit

---
+++ ferme.js
@@ -0,0 +1,11 @@
+
+let one=  ["j","r", "i"];
+let second=  ["a", "d", "n"];
+let table = [];
+
+    for(let i = 0; i<=one.length; i++){
+        tab.push(one[i])
+        tab.push(second[i])
+    }
+    console.log(tab.join(''));
+
